f10da708d1752dddc945d07e8470546f88f5f6ba
matt2tech <mlipsey@basecampcodingacademy.org>
2018-11-28 15:51:00 -0600

    Changed funtions to class and changed urls to match

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,10 @@ from . import views
 app_name = 'app'
 
 urlpatterns = [
-    path('', views.create, name='create'),
-    path('link/<short_code>/', views.show, name='show'),
-    path('<short_code>/', views.goto, name='goto')
+    # path('', views.create, name='create'),
+    # path('link/<short_code>/', views.show, name='show'),
+    # path('<short_code>/', views.goto, name='goto')
+    path('', views.Create.as_view(), name='create'),
+    path('link/<short_code>/', views.Show.as_view(), name='show'),
+    path('<short_code>/', views.Goto.as_view(), name='goto')
 ]
--- app/views.py
+++ app/views.py
@@ -1,25 +1,51 @@
 from django.shortcuts import render, redirect, reverse
 from app.models import Link
+from django.views import View
 
 # Create your views here.
-def create(request):
-    if request.method == 'GET':
+# def create(request):
+#     if request.method == 'GET':
+#         return render(request, 'app/create.html')
+#     elif request.method == 'POST':
+#         url = request.POST.get('url')
+#         link = Link.shorten(url)
+#         if link is None:
+#             return render(request, 'app/create.html', {'invalid_url': True}, status=422)
+#         else:
+#             return redirect('app:show', short_code=link.short_code)
+#     return render(request, 'app/create.html')
+
+# def show(request, short_code):
+#     return render(request, 'app/show.html', {'link': Link.find_by_short_code(short_code)})
+
+# def goto(request, short_code):
+#     link = Link.find_by_short_code(short_code)
+#     if link is None:
+#         return redirect('app:create')
+#     else:
+#         return redirect(link.original)
+
+class Create(View):
+    def get(self, request):
         return render(request, 'app/create.html')
-    elif request.method == 'POST':
+    
+    def post(self, request):
         url = request.POST.get('url')
         link = Link.shorten(url)
         if link is None:
             return render(request, 'app/create.html', {'invalid_url': True}, status=422)
         else:
             return redirect('app:show', short_code=link.short_code)
-    return render(request, 'app/create.html')
+        return render(request, 'app/create.html')
 
-def show(request, short_code):
-    return render(request, 'app/show.html', {'link': Link.find_by_short_code(short_code)})
+class Show(View):
+    def get(self, request, short_code):
+        return render(request, 'app/show.html', {'link': Link.find_by_short_code(short_code)})
 
-def goto(request, short_code):
-    link = Link.find_by_short_code(short_code)
-    if link is None:
-        return redirect('app:create')
-    else:
-        return redirect(link.original)
+class Goto(View):
+    def get(self, request, short_code):
+        link = Link.find_by_short_code(short_code)
+        if link is None:
+            return redirect('app:create')
+        else:
+            return redirect(link.original)
