c67df5eeb25754a1037522b827f6115ac6e34c95
matt2tech <mlipsey@basecampcodingacademy.org>
2018-11-28 14:46:23 -0600

    Added paths

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from . import views
 
 app_name = 'app'
 
-urlpatterns = []
+urlpatterns = [
+    path('', views.create, name='create'),
+    path('link/<short_code>/', views.show, name='show'),
+    path('<short_code>/', views.goto, name='goto')
+]
